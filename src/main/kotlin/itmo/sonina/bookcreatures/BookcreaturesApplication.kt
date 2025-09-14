3c21e0f81c023c470d6ca3aa2b97e69aef14891a
soniina <sonikpav@gmail.com>
2025-09-14 20:38:54 +0300

    init

---
+++ src/main/kotlin/itmo/sonina/bookcreatures/BookcreaturesApplication.kt
@@ -0,0 +1,11 @@
+package itmo.sonina.bookcreatures
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookcreaturesApplication
+
+fun main(args: Array<String>) {
+    runApplication<BookcreaturesApplication>(*args)
+}
---
+++ src/test/kotlin/itmo/sonina/bookcreatures/BookcreaturesApplicationTests.kt
@@ -0,0 +1,13 @@
+package itmo.sonina.bookcreatures
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookcreaturesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
